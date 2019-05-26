1f0fb564109b9666528f0824bb7ec1ea02dae46f
natan <natan.mendes.slice@estadao.com>
2019-05-26 19:44:24 -0300

    ajustes

---
+++ resources/views/pages/informacoes.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app', ['activePage' => 'typography', 'titlePage' => __('Typography')])
+
+@section('content')
+<div class="content">
+  <div class="container-fluid">
+    <div class="card">
+      <div class="card-header card-header-primary">
+        <h4 class="card-title">Gerenciamento de Estoque - Tibikos</h4>
+      </div>
+      <div class="card-body">
+        <div id="typography">
+          <div class="row">
+          <div class="col-md-12">
+
+          <h2>
+                Trabalho de Engenharia de software 3
+                </h2>
+                <h4>
+                <b>Integrantes dos grupo:</b>
+                </h4>
+              <p>
+              Henrick Hendricky RA 17201443
+              </p>
+              <p>
+
+              Isabella Maria RA 17201452
+              <p>
+              Natan Mendes RA 15113388
+              </p>
+              <p>
+              Willian Garbo RA 17201561
+              </p>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
