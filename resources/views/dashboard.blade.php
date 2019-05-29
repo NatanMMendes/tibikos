fb26f287532a797c4ca3e6fd837a640cd81673ab
natan <natan.mendes.slice@estadao.com>
2019-05-29 02:20:41 -0300

    ajustes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
           <div class="card card-stats">
             <div class="card-header card-header-warning card-header-icon">
               <div class="card-icon">
-                <i class="material-icons">content_copy</i>
+                <i class="material-icons">check_box_outline_blank</i>
               </div>
               <h3 class="card-title">Produtos
               <p><small>Consultar e Manter os produtos</small></p>
@@ -16,9 +16,8 @@
               </h3>
             </div>
             <div class="card-footer">
-                <a href>Consultar e Manter os produtos</a><br>
-                <a href>Consultar e Manter os produtos</a>
-
+                <a href="{{ route('produtos') }}">Consultar e Gerenciar</a><br>
+                <a href="{{ route('novo-produto') }}">Incluir Novo Produto</a>
             </div>
           </div>
         </div>
@@ -26,35 +25,34 @@
           <div class="card card-stats">
             <div class="card-header card-header-warning card-header-icon">
               <div class="card-icon">
-                <i class="material-icons">content_copy</i>
+                <i class="material-icons">store</i>
               </div>
-              <h3 class="card-title">Compras
+              <h3 class="card-title">Estoque
+              <p><small>Realizar consultas,devoluções,baixas  e movimentaçao</small></p>
+
               </h3>
             </div>
             <div class="card-footer">
-              <div class="stats">
-                <p>Consultar e Realizar Pedidos de Compras</p>
-              </div>
+                <a href="{{ route('estoque') }}">Gerenciar estoque</a><br>
             </div>
           </div>
         </div>
-     
       </div>
       <div class="row">
-      <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="col-lg-6 col-md-6 col-sm-6">
           <div class="card card-stats">
             <div class="card-header card-header-warning card-header-icon">
               <div class="card-icon">
-                <i class="material-icons">content_copy</i>
+                <i class="material-icons">supervisor_account</i>
               </div>
               <h3 class="card-title">Fornecedores
-                <p><small>Gerencimento e Controle</small></p>
+              <p><small>Consultar e Manter os Fornecedores</small></p>
+
               </h3>
             </div>
             <div class="card-footer">
-              <div class="stats">
-                <a href>Listagem</a>
-              </div>
+                <a href="{{ route('fornecedores') }}">Consultar e Gerenciar</a><br>
+                <a href="{{ route('novo-fornecedor') }}">Incluir Novo Produto</a>
             </div>
           </div>
         </div>
@@ -62,19 +60,54 @@
           <div class="card card-stats">
             <div class="card-header card-header-warning card-header-icon">
               <div class="card-icon">
-                <i class="material-icons">content_copy</i>
+                <i class="material-icons">shopping_cart</i>
               </div>
-              <h3 class="card-title">Localidades
+              <h3 class="card-title">Compras
+              <p><small>Consultar e Manter os Pedidos de Compra</small></p>
+
               </h3>
             </div>
             <div class="card-footer">
-              <div class="stats">
-                <p>Consultar e Manter localidades da loja</p>
-              </div>
+                <a href="{{ route('compras') }}">Consultar e Gerenciar</a><br>
+                <a href="{{ route('novo-compra') }}">Incluir Novo Pedido</a>
             </div>
           </div>
-        </div>   
+        </div>
       </div>
-    </div>
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div class="card card-stats">
+            <div class="card-header card-header-warning card-header-icon">
+              <div class="card-icon">
+                <i class="material-icons">room</i>
+              </div>
+              <h3 class="card-title">Locais
+              <p><small>Consultar e Manter os locais onde estão os produtos</small></p>
+
+              </h3>
+            </div>
+            <div class="card-footer">
+                <a href="{{ route('locais') }}">Consultar e Gerenciar</a><br>
+                <a href="{{ route('novo-local') }}">Incluir Novo Local</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div class="card card-stats">
+            <div class="card-header card-header-warning card-header-icon">
+              <div class="card-icon">
+                <i class="material-icons">person_outline</i>
+              </div>
+              <h3 class="card-title">Gerenciar Usuarios
+              <p><small>Controle de Acesso ao Sitema</small></p>
+
+              </h3>
+            </div>
+            <div class="card-footer">
+                <a href="/user">Painel de Usuarios</a><br>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 @endsection
--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -14,43 +14,44 @@
       </li>
       <li class="nav-item{{ $activePage == 'estoque' ? ' active' : '' }}">
         <a class="nav-link" href="{{ route('estoque') }}">
-          <i class="material-icons"></i>
+        <i class="material-icons">store</i>
             <p>{{ __('Estoque') }}</p>
         </a>
       </li>
       <li class="nav-item{{ $activePage == 'produtos' ? ' active' : '' }}">
         <a class="nav-link" href="{{ route('produtos') }}">
-          <i class="material-icons"></i>
+        <i class="material-icons">check_box_outline_blank</i>
             <p>{{ __('Produtos') }}</p>
         </a>
       </li>
       <li class="nav-item{{ $activePage == 'fornecedores' ? ' active' : '' }}">
         <a class="nav-link" href="{{ route('fornecedores') }}">
-          <i class="material-icons"></i>
+        <i class="material-icons">supervisor_account</i>
+
             <p>{{ __('Fornecedores') }}</p>
         </a>
       </li>
       <li class="nav-item{{ $activePage == 'compras' ? ' active' : '' }}">
         <a class="nav-link" href="{{ route('compras') }}">
-          <i class="material-icons"></i>
+        <i class="material-icons">shopping_cart</i>
             <p>{{ __('Compras') }}</p>
         </a>
       </li>
       <li class="nav-item{{ $activePage == 'locais' ? ' active' : '' }}">
         <a class="nav-link" href="{{ route('locais') }}">
-          <i class="material-icons"></i>
+        <i class="material-icons">room</i>
             <p>{{ __('Locais') }}</p>
         </a>
       </li>
       <li class="nav-item{{ $activePage == 'user' ? ' active' : '' }}">
         <a class="nav-link" href="/user">
-          <i class="material-icons"></i>
+        <i class="material-icons">person_outline</i>
             <p>{{ __('Usuarios') }}</p>
         </a>
       </li>
       <li class="nav-item{{ $activePage == 'informacoes' ? ' active' : '' }}">
         <a class="nav-link" href="{{ route('informacoes') }}">
-          <i class="material-icons"></i>
+        <i class="material-icons">dashboard</i>
             <p>{{ __('Informações') }}</p>
         </a>
       </li>
